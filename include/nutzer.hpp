c8fba6d5d57437c5fe1d652b401d964bedf0ed9c
Aicha <s28aerra@uni-bonn.de>
2023-06-21 13:30:38 +0200

    Hab die hpp fuer den Nutzer fertig gemacht

---
+++ include/nutzer.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+namespace handelsPlatz{
+class Nutzer{
+
+private:
+  int kontonummer;
+  vector<string> guetervorrat;
+  std::string benutzername;
+  std::string passwort;
+
+public:
+  Nutzer(int kontonummer_, std::string benutzername_, std::string passwort_)
+  : kontonummer(kontonummer_), benutzername(benutzername_), passwort(passwort_)
+  {}
+    
+  int getKontonummer();
+  vector<string> getGuetervorrat();
+  std::string getBenutzername();
+  std::string getPasswort();
+
+
+
+
+}
+}
