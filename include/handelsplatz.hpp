23f610f23f1520672f9e64dbcdc1c8295bf9af54
Imene Benamer belkacem <benamerbelkacemi0@>
2023-06-21 13:01:23 +0200

    neuerNutzer und login fkt geschrieben

--- include/handelsplatz.hpp
+++ include/handelsplatz.hpp
@@ -1,21 +1,29 @@
 #include <string>
 #include <iostream>
-
+#include <vector>
 using namespace std;
-namespace handelsPlatz{
+namespace handel{
 
     class handelsplatz
     {
     private:
+        std::map<string, string> localUser;
+        
+        //TODO: Wenn Aichas klasse fertig ist, wird das ausgeklammert
+        //std:std::vector<Nutzer> nutzersKlasse;
+
+        // TODO: void preisAnpassung(); in Struct reintun
+        
 
     public:
-        handelsplatz(/* args */);
+        handelsplatz();
         bool kaufen();
         bool verkaufen();
-        bool login(string name, int password);
+        bool login(string name, string password);
+        bool neuerNutzer(string name, int password);
         int handelswerteAbrufen();
 
-
+        int aktuellerHandelswert();
 
     };
     
--- src/handelsplatz.cpp
+++ src/handelsplatz.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 #include <string>
+#include <map>
 #include "../include/handelsplatz.hpp"
 
-namespace handelsplatz
+using namespace std;
+namespace handel
 {
+    handelsplatz::handelsplatz(){
+
+        
+
+    }
+    bool handelsplatz::neuerNutzer(string newName, string newPassword){
+        for (string [name, password] : localUser)
+            if (name == newName)
+                return false;
+
+        localUser[newName] = newPassword;
+
+        // TODO: Wenn aichas Klasse fertig ist, auskommentieren
+        //handel::Nutzer newUser = handel::Nutzer(newName, newPassword);
+        //nutzersKlasse.push_back(newUser);
+        return true;
+    }
+
+    bool handelsplatz::login(string userName, string userPassword){
+       for (string [name, password] : localUser)
+            if (name == userName && password == userPassword)
+                // Wenn Aicha: Nutzer anstatt bool zurückgeben
+                return true;
+
+        return false;
+    }
+
+
 
     bool handelsplatz::kaufen()
     {
