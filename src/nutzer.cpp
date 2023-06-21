d4d944d8625dbb7af53de411238465fc52b1111f
Aicha <s28aerra@uni-bonn.de>
2023-06-21 13:44:50 +0200

    cpp fuer den Nutzer angefangen

---
+++ src/nutzer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector> 
+#include "../include/nutzer.hpp"
+
+using namespace std;
+namespace market{
+
+    int nutzer::getKontonummer() const{
+        return kontonummer;
+    }
+
+    std::string nutzer::getBenutzername() const{
+        return benutzername:
+    }
+
+    std::string nutzer::getPasswort() cosnt{
+        return passwort;
+    }
+ 
+    //getGuetervorrat fkt noch machen
+
+
+
+}
