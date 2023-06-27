3fba004ca63cb1ab33f8ff6c462096f3bdfc5b73
Aicha <s28aerra@uni-bonn.de>
2023-06-28 00:48:25 +0200

    Verbesserungen an allen funktionen gemacht und die removeHandelsgueter funktion hinzugefuegt

--- include/nutzer.hpp
+++ include/nutzer.hpp
@@ -21,55 +21,19 @@ namespace market
         nutzer(){};
         
         int getKontostand() const;
+
         void setKontostand(int neu);
 
+        bool hatHandelsgut(string name);
+      
+        int handelsgutAnzahl(string name);
 
-        // TODO: alles auf den vector: guetervorrat bezogen
-        // TODO: {return true;} entfernen
-        //iteriert durch den vector guettervorrat und guckt ob name drin ist wenn ja dann returnt er true
-        //sonst false
-        bool hatHandelsgut(string name){
-          for(i=0, i<=guetervorrat.size(),i++){
-            if(i==name){
-              return true;
-            }
-            else{ return false;}
-          }
-        };
-        
-
-        //TODO: erst hatHandelsgut aufrufen UND {return 0;} löschen
-        // dann checken, wie viele Handelsgüter es gibt
-        //ruft die funktion hatHandelsgut auf, es wird geguckt ob wir den handelsgut haben, wenn nicht 
-        // returnt er null, in dieser funktion wird geguckt wie viele von dem handelsgut es gibt man nimmt 
-        // handelsgut getanzahl, sonst null
-        int handelsgutAnzahl(string handelsgut){
-          hatHandelsgut();
-          if(true){
-            handelsgut.getAnzahl();
-          }
-          else{return null;}
-        };
-
-        // erst überprüfen, ob hatHandeslgut, dann anzahl erhöhen, sonst neu erstellen und guetervorrat adden
-        // TODO: {return true;} entfernen, weil fkt soll ja neu geschrieben werden
-        bool addHandeslguterVorrat(string name, int anzahl){
-          hatHandelsgut();
-          if(true){
-            name.setAnzahl();
-          }
-          else{
-            name_=name;
-            anzahl_=anzahl;
-          }
-        };
+        bool addHandeslguterVorrat(string name, int anzahl);
     
-
-        // Überprüfen, gibt es Handelsgut überhaupt.
-        // Wenn ja, dann abziehen und wenn anzahl == 0, dann auch löschen
-        bool removeHandeslguterVorrat(string name, int anzahl){return true;};
+        bool removeHandeslguterVorrat(string name, int anzahl);
 
         std::string getBenutzername() const;
+        
         std::string getPasswort() const; 
     };
 
--- src/nutzer.cpp
+++ src/nutzer.cpp
@@ -26,4 +26,71 @@ std::string nutzer::getPasswort() const
     return passwort;
 }
 
-// getGuetervorrat fkt noch machen
+//iteriert durch den vector guettervorrat und guckt ob name drin ist wenn ja dann returnt er true
+//sonst false
+bool nutzer::hatHandelsgut(string name) const{
+          for(int i=0;i<=guetervorrat.size();i++){
+            if(guetuervorrat[i].name==name){
+              return true;
+            }
+            else{ return false;}
+          }
+        }
+
+ //ruft die funktion hatHandelsgut auf, es wird geguckt ob wir den handelsgut haben, wenn nicht 
+ // returnt er null, in dieser funktion wird geguckt wie viele von dem handelsgut es gibt man nimmt 
+// handelsgut getanzahl, sonst null
+int nutzer::handelsgutAnzahl(string name) const{
+          if(hatHandelsgut(name)==true){
+            for(int i=0;i<=guetervorrat.size();i++){
+                if(guetuervorrat[i].name==name){
+                    return guetuervorrat[i].getAnzahl();
+                }
+
+            }
+          }
+          else{return null;}
+        }   
+
+
+// erst überprüfen, ob hatHandeslgut, dann anzahl erhöhen, sonst neu erstellen und guetervorrat adden
+void nutzer::addHandeslguterVorrat(string name, int anzahl)const{
+          if(hatHandelsgut()==true){
+            for(int i=0;i<=guetervorrat.size();i++){
+                if(guetuervorrat[i].name==name){
+                    guetervorrat[i].setAnzahl(guetervorrat[i].getAnzahl()+anzahl);
+                }
+
+            }
+          }
+          else{
+            name_=name;
+            anzahl_=anzahl;
+            guetervorrat.add(new Handelsgueter(name, anzahl));
+            
+          }
+        }
+
+ // Überprüfen, gibt es Handelsgut überhaupt.
+ // Wenn ja, dann abziehen und wenn anzahl == 0, dann auch löschen
+void nutzer::removeHandeslguterVorrat(string name, int anzahl){
+         
+        if(hatHandelsgut()==true){
+         for(int i=0;i<=guetervorrat.size();i++){
+                if(guetuervorrat[i].anzahl>=anzahl && guetuervorrat[i].name==name){
+                    guetervorrat[i].getAnzahl()-anzahl;
+                    return guetervorrat[i];
+                }
+         
+            else{std::cout << "Sie besitzen nicht so viele gueter" << std::endl;;}
+         }
+         else{return false}
+}
+}
+
+/*vector<Handelsgueter> getHandelsgueter{
+
+    return vector<Handelsgueter> guetervorrat; 
+}
+*/
+    
