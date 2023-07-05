1970f2a8cda46face04297d4d2086a4a74514b73
Aicha <s28aerra@uni-bonn.de>
2023-07-05 13:01:55 +0200

    Unittest angefangen und die erste Funktion getGueterVorratName getestet

---
+++ test/nutzerTest.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include "../src/nutzer.cpp"
+#include "../src/handelsgueter.cpp"
+#include <string>
+#include <iostream>
+
+using namespace handelsplatz;
+// cmake -S . -B build && cmake --build build
+
+TEST(nutzer, getGuterVorratName){
+    nutzer user;
+    // Add test data to the 'guetervorrat' member variable of 'user'
+
+    std::vector<std::string> expectedNames = {"name1", "name2", "name3"};
+    std::vector<std::string> actualNames = user.getGuterVorratName();
+
+    EXPECT_EQ(expectedNames, actualNames) << "Die Gueter wurden nicht gefunden";
+}
