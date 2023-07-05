20e26d140958e831c0a6d43542278faa7a8d5ca0
Aicha <s28aerra@uni-bonn.de>
2023-07-05 17:14:14 +0200

    HandelsgueterTest getNAme fkt

---
+++ test/handelsgueterTest.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include "../src/nutzer.cpp"
+#include "../src/handelsgueter.cpp"
+#include <string>
+#include <iostream>
+
+using namespace handelsplatz;
+// cmake -S . -B build && cmake --build build
+
+TEST(Handelsgueter, getName)
+{
+    Handelsgueter handelsgut = Handelsgueter("kamera",8);
+    // Set test data for the 'name' member variable of 'handelsgut'
+
+    std::string expectedName = "kamera";
+    std::string actualName = handelsgut.getName();
+
+    EXPECT_EQ(expectedName, actualName);
+}
+
